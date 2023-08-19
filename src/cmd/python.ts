40de817065d99dd2612c4414fa901e8a0ad0954d
tadashi-aikawa <syou.maman@gmail.com>
2023-08-19 14:43:59 +0900

    Support Python3.10,3.9,3.8

--- src/cmd/python.ts
+++ src/cmd/python.ts
@@ -14,7 +14,7 @@ import { cd, cp, loading, run } from "../utils/command-wrapper";
 const BASE_PATH = path.join(os.homedir(), "tmp", "sandbox");
 const FALCON_TEMPLATE_ROOT = path.join(__dirname, "..", "..", "template");
 
-type ProjectType = "python3-latest";
+type ProjectType = "python3-latest" | "python3.10" | "python3.9" | "python3.8";
 
 export async function exec() {
   intro(`Let's create a Python project!`);
@@ -22,7 +22,12 @@ export async function exec() {
   const projectName = await text({ message: "Project name?" });
   const projectType = (await select({
     message: "Python type?",
-    options: [{ value: "python3-latest", label: "Python3 latest" }],
+    options: [
+      { value: "python3-latest", label: "Python3 latest" },
+      { value: "python3.10", label: "Python3.10" },
+      { value: "python3.9", label: "Python3.9" },
+      { value: "python3.8", label: "Python3.8" },
+    ],
   })) satisfies ProjectType | symbol;
 
   if (isCancel(projectName) || isCancel(projectType)) {
@@ -32,25 +37,37 @@ export async function exec() {
 
   switch (projectType) {
     case "python3-latest":
-      await createPython3Latest(projectName);
+      await createPython3(projectName, "python3");
+      break;
+    case "python3.10":
+      await createPython3(projectName, "python310");
+      break;
+    case "python3.9":
+      await createPython3(projectName, "python39");
+      break;
+    case "python3.8":
+      await createPython3(projectName, "python38");
       break;
   }
 }
 
-async function createPython3Latest(projectName: string) {
-  const projectType: ProjectType = "python3-latest";
+async function createPython3(projectName: string, pythonCommand: string) {
   const projectRoot = path.join(BASE_PATH, "python", projectName);
   const templateRoot = path.join(FALCON_TEMPLATE_ROOT, "python");
 
   await cp(templateRoot, projectRoot);
   await cd(projectRoot, { createDirectoryIfNotExists: true });
-  await loading(`Create virtual environment`, "python3 -m venv .venv");
+  await loading(`Create virtual environment`, `${pythonCommand} -m venv .venv`);
+  const venvScripts = path.join(".venv", "Scripts");
+
+  const pip = path.join(venvScripts, "pip");
+  await loading(`Install dependencies`, `${pip} install -r requirements.txt`);
 
   log.info("Run");
-  const venvScripts = path.join(".venv", "Scripts");
   const python = path.join(venvScripts, "python");
   await run(`${python} main.py`);
 
   log.success("Success!");
   outro(`cd ${projectRoot}`);
+  outro(`.venv/Scripts/activate`);
 }
